e9ddfe4da233677a785a16423fedbc1694d032b6
Trauvin <mateusgalote@gmail.com>
2021-12-03 18:27:51 -0300

    swiper-slide

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import initAccordion from "./modules/info-stoic.js";
 import initQuoute from "./modules/quote.js";
+import initSwiper from "./modules/swiper.js";
 
 initAccordion();
 initQuoute();
+initSwiper();
